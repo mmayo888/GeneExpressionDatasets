f9c21d00cd97afc10190f83a5e89463e32633f7a
Michael Mayo <mmayo@waikato.ac.nz>
2017-05-15 09:09:47 +1200

    added weka monolithic version for running experiments

--- get-datasets.R
+++ get-datasets.R
@@ -11,41 +11,41 @@ library(foreign)
 # https://www.ncbi.nlm.nih.gov/sites/GDSbrowser
 
 # lung adenocarcinoma dataset
-# if (!exists("lung"))
-#   lung <- show(getGEO("GDS3257")) 
-# data<-lung@table
-# data<-t(data[,3:ncol(data)])
-# data<-data.frame(data,lung@columns$tissue)
+ if (!exists("lung"))
+   lung <- show(getGEO("GDS3257")) 
+ data<-lung@table
+ data<-t(data[,3:ncol(data)])
+ data<-data.frame(data,lung@columns$tissue)
 # write.arff(data,"lung.arff")
 
 # myelodysplastic syndrome dataset
-# if (!exists("myelo"))
-#   myelo <- show(getGEO("GDS3795")) 
-# data<-myelo@table
-# data<-t(data[,3:ncol(data)])
-# data<-data.frame(data,myelo@columns$disease.state)
+ if (!exists("myelo"))
+   myelo <- show(getGEO("GDS3795")) 
+ data<-myelo@table
+ data<-t(data[,3:ncol(data)])
+ data<-data.frame(data,myelo@columns$disease.state)
 # write.arff(data,"myelo.arff")
 
 # pulmanory hypertension dataset
-# if (!exists("pulm"))
-#   pulm <- show(getGEO("GDS4549")) 
-# data<-pulm@table
-# data<-t(data[,3:ncol(data)])
-# data<-data.frame(data,pulm@columns$disease.state)
+ if (!exists("pulm"))
+   pulm <- show(getGEO("GDS4549")) 
+ data<-pulm@table
+ data<-t(data[,3:ncol(data)])
+ data<-data.frame(data,pulm@columns$disease.state)
 # write.arff(data,"pulm.arff")
 
 # pancreatic dataset
-# if (!exists("panc"))
-#   panc <- show(getGEO("GDS4103")) 
-# data<-panc@table
-# data<-t(data[,3:ncol(data)])
-# data<-data.frame(data,panc@columns$tissue)
+ if (!exists("panc"))
+   panc <- show(getGEO("GDS4103")) 
+ data<-panc@table
+ data<-t(data[,3:ncol(data)])
+ data<-data.frame(data,panc@columns$tissue)
 # write.arff(data,"panc.arff")
 
 # psoriais dataset
-# if (!exists("psor"))
-#   psor <- show(getGEO("GDS4600")) 
-# data<-psor@table
-# data<-t(data[,3:ncol(data)])
-# data<-data.frame(data,psor@columns$disease.state)
+ if (!exists("psor"))
+   psor <- show(getGEO("GDS4600")) 
+ data<-psor@table
+ data<-t(data[,3:ncol(data)])
+ data<-data.frame(data,psor@columns$disease.state)
 # write.arff(data,"psor.arff")
